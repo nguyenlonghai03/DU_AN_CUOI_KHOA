6b916a4dd90440bb1e54a2d627a01498b980c36d
trang <trangps01508@gmail.com>
2020-11-26 19:15:07 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-<<<<<<< HEAD
 import React from "react";
 import "./assets/css/main.css";
 import "./App.css";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ChiTietPhim from "./pages/ChiTietPhim/ChiTietPhim";
-// import TrangChu from "./pages/TrangChu/TrangChu";
 import { HomeTemplate } from "./templates/HomeTemplate";
 import DangNhap from "./pages/TrangChu/DangNhap";
 import DangKy from "./pages/TrangChu/DangKy";
 import TrangChu from "./pages/TrangChu/TrangChu";
 import { UserTemplate } from "./templates/UserTemplate";
-=======
-import React from 'react';
-import './assets/css/main.css'
-import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import ChiTietPhim from './pages/ChiTietPhim/ChiTietPhim';
-import TrangChu from './pages/TrangChu/TrangChu';
-import { HomeTemplate } from './templates/HomeTemplate';
-import PhongVe from './pages/PhongVe/PhongVe';
-
->>>>>>> 3f8070464ef71750e65a148c4380ec79e5f80240
+import PhongVe from "./pages/PhongVe/PhongVe";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <HomeTemplate exact path="/home" Component={TrangChu} />
-        <HomeTemplate exact path="/chitietphim/::maPhim" Component={ChiTietPhim} />
-        <HomeTemplate exact path='/phongve/:maLichChieu' Component={PhongVe} />
+        <HomeTemplate
+          exact
+          path="/chitietphim/::maPhim"
+          Component={ChiTietPhim}
+        />
+        <HomeTemplate exact path="/phongve/:maLichChieu" Component={PhongVe} />
 
         <HomeTemplate exact path="/" Component={TrangChu} />
 
